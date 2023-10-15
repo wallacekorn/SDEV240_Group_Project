370b217bfd49b08c81fa1a06e98317c222cad6ee
gseaburn <147951723+gseaburn@users.noreply.github.com>
2023-10-14 20:53:03 -0400

    Fixed dark mode. Fixed font issue.
    Fixed dark mode to allow it to change between light and dark. Font constraints have been implemented, although the font sizes keep displaying higher font sizes than what has been allowed.

---
+++ SDEV240_Group_Project-main/MainForm.cs
@@ -0,0 +1,183 @@
+/*
+Still need to add header comments
+
+
+*/
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+
+namespace Materials_List_Estimator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FileCategory_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void ViewCategory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog anOpenFileDialog = new OpenFileDialog(); // opens the 'select a file' window
+
+            if (anOpenFileDialog.ShowDialog() == DialogResult.OK) // if something is entered
+            {
+                string openedFileName = anOpenFileDialog.FileName; // assigns its filename to a var
+
+
+
+                // still need to call a method that sorts the files data to the data grid - or remove this option
+            }
+        }
+
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog theSaveFileDialog = new SaveFileDialog(); // opens save dialog
+            theSaveFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*"; // suggested file format - we want csv
+            theSaveFileDialog.Title = "Save Materials List"; // title for the window
+            theSaveFileDialog.FileName = "Materials_Estimate.csv"; // sets a suggested filename
+            theSaveFileDialog.FilterIndex = 1; // sets it so the csv option is selected first
+
+
+            if (theSaveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = theSaveFileDialog.FileName;
+                SaveToCSV(theDataGridView, fileName);
+            }
+
+        }
+
+        private void ClearGrid_Click(object sender, EventArgs e)
+        {
+            // clear table values
+            this.theDataGridView.Rows.Clear();
+        }
+
+        private void FontMenuSubgroup_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+
+            if (fontDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                // Get the selected font
+                Font selectedFont = fontDialog.Font;
+
+                // Set constraints for the font size
+                float minFontSize = 8.0f;
+                float maxFontSize = 14.0f;
+
+                // Apply constraints to font size
+                float constrainedFontSize = Math.Max(minFontSize, Math.Min(selectedFont.Size, maxFontSize));
+                selectedFont = new Font(selectedFont.FontFamily, constrainedFontSize, selectedFont.Style);
+
+                this.Font = selectedFont;
+            }
+        }
+
+
+        private bool isDarkMode = false; // Initialize with light mode
+
+        private void DarkModeToggle_Click(object sender, EventArgs e)
+        {
+            isDarkMode = !isDarkMode;
+            ApplyDarkMode();
+        }
+
+        private void ApplyDarkMode()
+        {
+            if (isDarkMode)
+            {
+                // Dark Mode
+                this.BackColor = Color.FromArgb(80, 80, 80); // Set the background color to a dark shade
+                this.ForeColor = Color.White; // Set text and foreground color to a light color
+                theDataGridView.BackgroundColor = Color.FromArgb(50, 50, 50);
+                theDataGridView.ForeColor = Color.LightSlateGray;
+            }
+            else
+            {
+                // Light Mode
+                this.BackColor = Color.White; // Set the background color back to light
+                this.ForeColor = Color.Black; // Set text and foreground color back to dark
+                theDataGridView.BackgroundColor = this.BackColor;
+                theDataGridView.ForeColor = this.ForeColor;
+            }
+        }
+
+
+        public static void SaveToCSV(DataGridView theDataGridView, string fileName)
+        {
+
+            StreamWriter streamWriter = new StreamWriter(fileName);
+
+            // This loop processes the headers of the datagridview columns
+            for (int i = 0; i < theDataGridView.Columns.Count; ++i)
+            {
+                streamWriter.Write(theDataGridView.Columns[i].HeaderText); // Writes the column headers to the stream
+                if (i != theDataGridView.Columns.Count - 1) // this prevents the last column from getting a comma
+                {
+                    streamWriter.Write(","); // adds comma delimiter
+                }
+            }
+            streamWriter.Write(streamWriter.NewLine); // breaks the entry to the next line
+
+
+            // This loop processes the body of the datagridview columns
+            for (int iRows = 0; iRows < theDataGridView.Rows.Count; ++iRows)
+            {
+                if (theDataGridView.Rows[iRows].IsNewRow) // this makes it so the last (and always blank) row isn't entered as all commas
+                    continue;
+                for (int iCells = 0; iCells < theDataGridView.Columns.Count; ++iCells)
+                {
+                    streamWriter.Write(theDataGridView.Rows[iRows].Cells[iCells].Value);
+                    if (iCells != theDataGridView.Columns.Count - 1) // this prevents the last column from getting a comma
+                    {
+                        streamWriter.Write(","); // adds comma delimiter
+                    }
+                }
+                streamWriter.Write(streamWriter.NewLine); //Breaks the entry to the next line
+            }
+            streamWriter.Close();
+        }
+
+
+        // This method adds the "Cost" column and grand totals the value into the calTotalTextBox.
+        private void CalcTotal_Click(object sender, EventArgs e)
+        {
+            double accumulatedTotal = 0;
+            for (int i = 0; i < theDataGridView.Rows.Count; ++i)
+            {
+                accumulatedTotal += Convert.ToDouble(theDataGridView.Rows[i].Cells["entryCostColumn"].Value);
+            }
+            calcTotalTextBox.Text = accumulatedTotal.ToString("C", CultureInfo.CurrentCulture); // Assigns the value to the output box in currency 
+        }
+
+        private void compareButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
